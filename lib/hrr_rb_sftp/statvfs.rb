d583a745e4d8aecdce5189006baaab37109c40bc
hirura <hirura@gmail.com>
2020-06-20 20:41:19 +0900

    update top level to require sys-filesystem and hrr_rb_sftp

--- lib/hrr_rb_sftp/statvfs.rb
+++ lib/hrr_rb_sftp/statvfs.rb
@@ -1,8 +1,8 @@
-require "hrr_rb_sftp/statvfs/version"
-
 module HrrRbSftp
   module Statvfs
-    class Error < StandardError; end
-    # Your code goes here...
   end
 end
+
+require "sys/filesystem"
+require "hrr_rb_sftp"
+require "hrr_rb_sftp/statvfs/version"
